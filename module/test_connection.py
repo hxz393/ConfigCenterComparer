b01279d6db6958ff6743b62b8ed50556f429a7d6
hxz393 <hxz393@gmail.com>
2023-11-14 11:09:25 +0800

    add file

---
+++ module/test_connection.py
@@ -0,0 +1,51 @@
+"""
+这是一个用于测试数据库和SSH连接的Python模块。
+
+本模块提供了 `test_connection` 函数，用于测试指定配置的MySQL数据库连接和SSH连接。
+该函数接受一个包含数据库和SSH连接配置信息的字典参数，返回每个环境测试结果的列表。
+
+主要用途是验证不同环境下的数据库和SSH连接状态。
+
+:author: assassing
+:contact: https://github.com/hxz393
+:copyright: Copyright 2023, hxz393. 保留所有权利。
+"""
+
+import logging
+from typing import Optional, Dict, Any, List
+
+from lib.mysql_query import mysql_query
+from lib.mysql_query_with_ssh import mysql_query_with_ssh
+from lib.ssh_test import ssh_test
+from config.settings import SQL_TEST_MYSQL
+
+# 初始化日志记录器
+logger = logging.getLogger(__name__)
+
+
+def test_connection(config_connection: Dict[str, Dict[str, Any]]) -> Optional[List[Dict[str, Any]]]:
+    """
+    测试数据库和SSH连接。
+
+    通过提供的配置信息，此函数测试MySQL数据库连接和SSH连接。每个环境的测试结果将被收集并返回。
+
+    :param config_connection: 包含每个环境的数据库和SSH连接配置信息的字典。
+    :type config_connection: Dict[str, Dict[str, Any]]
+    :return: 包含每个环境测试结果的列表。如果测试成功，返回测试结果；如果发生错误，返回 None。
+    :rtype: Optional[List[Dict[str, Any]]]
+    """
+    try:
+        test_result = []
+        for env_name, config in config_connection.items():
+            ssh_test_result = ssh_test(config['ssh']) if config.get('ssh_on') else None
+            mysql_test_result = None
+
+            if config.get('mysql_on'):
+                mysql_test_result = mysql_query_with_ssh(config['ssh'], config['mysql'], SQL_TEST_MYSQL) if config.get('ssh_on') else mysql_query(config['mysql'], SQL_TEST_MYSQL)
+
+            test_result.append({'env_name': env_name, 'ssh_test_result': ssh_test_result, 'mysql_test_result': mysql_test_result})
+
+        return test_result
+    except Exception as e:
+        logger.exception(f"Error during connection test: {e}")
+        return None
---
+++ ui/action_test.py
@@ -0,0 +1,121 @@
+"""
+这是一个用于网络连接测试和状态显示的Python模块。
+
+此模块包含两个主要类：`ActionTest` 和 `TestRun`。`ActionTest` 类负责初始化测试动作，并提供用户界面交互功能，允许用户检查和显示网络连接状态。`TestRun` 类作为一个线程，用于后台检查网络连接状态。
+
+本模块的主要目的是提供一个简洁有效的方式来处理网络连接测试和状态显示。
+
+:author: assassing
+:contact: https://github.com/hxz393
+:copyright: Copyright 2023, hxz393. 保留所有权利。
+"""
+
+import logging
+
+from PyQt5.QtCore import QThread, pyqtSignal
+from PyQt5.QtGui import QIcon
+from PyQt5.QtWidgets import QAction
+
+from ConfigCenterComparer import ConfigCenterComparer
+from lib.get_resource_path import get_resource_path
+from module.config_init import config_init
+from module.test_connection import test_connection
+from .message_show import message_show
+
+# 初始化日志记录器
+logger = logging.getLogger(__name__)
+
+
+class ActionTest:
+    """
+    负责处理网络连接测试的类。
+
+    此类封装了网络测试的界面逻辑，包括初始化测试菜单项、绑定相关事件以及展示测试结果。
+
+    :param main_window: 主窗口对象，提供界面元素和语言设置。
+    :type main_window: ConfigCenterComparer
+    """
+
+    def __init__(self, main_window: ConfigCenterComparer):
+        """
+        初始化网络连接测试动作。
+
+        :param main_window: 主窗口对象，用于访问界面元素和语言设置。
+        :type main_window: ConfigCenterComparer
+        """
+        self.main_window = main_window
+        self.label_status = self.main_window.get_elements('label_status')
+        self.lang = self.main_window.get_elements('lang')
+
+        self.action_test = QAction(QIcon(get_resource_path('media/icons8-computers-connecting-26.png')), self.lang['ui.action_test_1'], self.main_window)
+        self.action_test.setShortcut('F9')
+        self.action_test.setStatusTip(self.lang['ui.action_test_2'])
+        self.action_test.triggered.connect(self.test)
+
+    def test(self) -> None:
+        """
+        触发网络连接测试。
+
+        此方法将禁用测试动作，并在状态栏显示正在测试的信息。
+        """
+        self.action_test.setEnabled(False)
+        self.label_status.setText(self.lang['ui.action_test_7'])
+
+        self.test_run = TestRun()
+        self.test_run.signal.connect(self.show_result_message)
+        self.test_run.start()
+
+    def show_result_message(self, test_result) -> None:
+        """
+        显示网络连接测试结果。
+
+        :param test_result: 测试结果列表，每个元素包含环境名称、SSH测试结果和MySQL测试结果。
+        :type test_result: list
+        """
+        self.action_test.setEnabled(True)
+        self.label_status.setText(self.lang['ui.action_test_8'])
+
+        if not test_result:
+            message_show('Warning', self.lang['ui.action_test_3'])
+            return
+
+        message_info = ''.join(
+            f'{result["env_name"]} {self.lang["ui.action_test_4"]}'
+            f'<span style="color: {"green" if result["ssh_test_result"] == 0 else "red"};">'
+            f'{"SSH " + self.lang["ui.action_test_5"] if result["ssh_test_result"] == 0 else "SSH " + self.lang["ui.action_test_6"]}</span> '
+            f'<span style="color: {"green" if result["mysql_test_result"] else "red"};">'
+            f'{"MySQL " + self.lang["ui.action_test_5"] if result["mysql_test_result"] else "MySQL " + self.lang["ui.action_test_6"]}</span><br>'
+            for result in test_result
+        )
+        message_show('Information', message_info)
+
+
+class TestRun(QThread):
+    """
+    用于后台执行网络连接测试的线程类。
+
+    此类继承自 QThread，在后台执行网络连接测试。测试完成后，它会发出信号以通知主线程测试结果。
+
+    使用 PyQt 的信号-槽机制来进行线程间的通信。
+    """
+    signal = pyqtSignal(list)
+
+    def __init__(self):
+        """
+        初始化网络连接测试线程。
+        """
+        super().__init__()
+
+    def run(self) -> None:
+        """
+        执行网络连接测试的主要逻辑。
+
+        此方法在线程启动时被调用。它执行网络连接测试，并通过信号发送测试结果。
+        """
+        try:
+            _, config_connection = config_init()
+            test_result = test_connection(config_connection)
+        except Exception as e:
+            logger.exception(f'Error during testing: {e}')
+            test_result = None
+        self.signal.emit(test_result)
