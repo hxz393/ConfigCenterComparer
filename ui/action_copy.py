99e504b63176958735e0aa39afa4c0a53726c653
hxz393 <hxz393@gmail.com>
2023-11-15 18:29:46 +0800

    123

--- ui/action_copy.py
+++ ui/action_copy.py
@@ -19,6 +19,7 @@ from lib.get_resource_path import get_resource_path
 
 logger = logging.getLogger(__name__)
 
+
 class ActionCopy:
     """
     处理配置中心比较器中的复制动作的类。
@@ -47,6 +48,14 @@ class ActionCopy:
         self.action_copy.triggered.connect(self.copy_selected)
 
     def copy_selected(self) -> Optional[str]:
+        """
+        执行选定内容的复制操作。
+
+        此方法触发复制选定范围的表格数据到剪贴板的流程。若没有选定的范围，则不进行任何操作。
+
+        :return: 复制到剪贴板的文本数据，如果没有数据被复制，则返回 None。
+        :rtype: Optional[str]
+        """
         try:
             selected_ranges = self.table.selectedRanges()
             if not selected_ranges:
@@ -61,12 +70,32 @@ class ActionCopy:
             return None
 
     def _format_selected_data(self, selected_ranges: List[QTableWidgetSelectionRange]) -> str:
+        """
+        格式化选定范围的表格数据为字符串。
+
+        该方法遍历所有选定的表格区域，将数据转换为文本格式。
+
+        :param selected_ranges: 选定的表格区域列表。
+        :type selected_ranges: List[QTableWidgetSelectionRange]
+        :return: 格式化后的字符串，用于复制到剪贴板。
+        :rtype: str
+        """
         rows_data = []
         for selected_range in selected_ranges:
             rows_data.extend(self._extract_range_data(selected_range))
         return '\n'.join(rows_data).strip()
 
     def _extract_range_data(self, selected_range: QTableWidgetSelectionRange) -> List[str]:
+        """
+        从选定的表格区域中提取数据。
+
+        此方法遍历选定区域内的每一行，如果行未被隐藏，则调用 `_extract_row_data` 方法来获取该行的数据。
+
+        :param selected_range: 用户在表格中选定的区域。
+        :type selected_range: QTableWidgetSelectionRange
+        :return: 选定区域中每行的数据组成的列表。
+        :rtype: List[str]
+        """
         range_data = []
         for row in range(selected_range.topRow(), selected_range.bottomRow() + 1):
             if not self.table.isRowHidden(row):
@@ -74,6 +103,18 @@ class ActionCopy:
         return range_data
 
     def _extract_row_data(self, row: int, selected_range: QTableWidgetSelectionRange) -> List[str]:
+        """
+        从表格的指定行中提取数据。
+
+        此方法遍历指定行内的每一列，如果列未被隐藏，则获取该单元格的文本。
+
+        :param row: 指定要提取数据的行号。
+        :type row: int
+        :param selected_range: 用户在表格中选定的区域。
+        :type selected_range: QTableWidgetSelectionRange
+        :return: 指定行中每个单元格的数据组成的列表。
+        :rtype: List[str]
+        """
         row_data = []
         for col in range(selected_range.leftColumn(), selected_range.rightColumn() + 1):
             if not self.table.isColumnHidden(col):
